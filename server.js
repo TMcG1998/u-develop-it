4c24e4b374754215d1446803882a9a35823655ee
Tommy McG <thomasjmcgovern98@gmail.com>
2022-05-04 16:15:32 -0400

    aa

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: '70MMY123a!',
+      password: '',
       database: 'election'
     },
     console.log('Connected to the election database.')
